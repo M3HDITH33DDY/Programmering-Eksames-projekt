235b0ff53c419273f0c790799511313b353e62e9
OliverDolle <oliver2006nielsen@gmail.com>
2025-05-08 15:06:00 +0200

    Opdateret så PDF virker i exe

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ from IMV.main_window import MainWindow
 def main():
     app = QApplication(sys.argv)
     app.setStyle("Fusion")
-    window = MainWindow()
+    """window = MainWindow()
     window.show()
-    sys.exit(app.exec())
+    sys.exit(app.exec())"""
     #Fejl ved opstart, til exe, ikke under udvikling
-    """try:
+    try:
         window = MainWindow()
         window.show()
         sys.exit(app.exec())
     except Exception as e:
         error_msg = traceback.format_exc()
         QMessageBox.critical(None, "Fatal Error", f"An unexpected error occurred:\n{error_msg}")
-        sys.exit(1)"""
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
